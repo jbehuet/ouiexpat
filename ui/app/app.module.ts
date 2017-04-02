76e3ad7b0149a17389c3b539ce6a207ee67844f7
jbehuet <jbehuet@gmail.com>
2017-04-02 23:48:51 +0200

    Prepare ui and create auth module

--- ui/app/app.module.ts
+++ ui/app/app.module.ts
@@ -1,20 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MaterializeModule } from 'angular2-materialize';
+import { AuthModule } from './auth/auth.module';
+
+import { ROUTES } from './app.routes';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        MaterializeModule,
+        RouterModule.forRoot(ROUTES),
+        AuthModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ ui/app/auth/auth.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { AuthComponent } from './auth.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+
+@NgModule({
+    imports: [
+        CommonModule,
+        RouterModule.forChild([
+            {
+                path: 'auth',
+                component: AuthComponent,
+                children: [
+                    { path: 'login', component: LoginFormComponent }
+                ]
+            }
+        ])
+    ],
+    declarations: [AuthComponent, LoginFormComponent]
+})
+export class AuthModule { }
