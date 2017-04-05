b90ee6e116fb6ebe06e42622e478c0a5567580ad
jbehuet <jbehuet@gmail.com>
2017-04-05 16:50:50 +0200

    Add angular-rollbar

--- ui/app/app.module.ts
+++ ui/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HttpModule } from '@angular/http';
+import { RollbarModule, RollbarService } from 'angular-rollbar/lib/';
 
 import { AuthModule } from './auth/auth.module';
 import { DashboardModule } from './dashboard/dashboard.module';
@@ -21,11 +22,14 @@ import { AppComponent } from './app.component';
     imports: [
         BrowserModule,
         HttpModule,
+        RollbarModule.forRoot({
+            accessToken: 'f548a573433d41a680fc4b738428f31d'
+        }),
         RouterModule.forRoot(ROUTES),
         AuthModule,
         DashboardModule
     ],
-    providers: [CookieService, AuthGuard, AuthenticationService],
+    providers: [RollbarService, CookieService, AuthGuard, AuthenticationService],
     bootstrap: [AppComponent]
 })
 export class AppModule { }
