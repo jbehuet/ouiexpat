eea3cecf2894c0a2a3725fcf0d6606b2410b7687
cDaffe <atakikoo@gmail.com>
2017-05-01 18:04:51 +0200

    replace happyexpat component by router-outlet to be able to access blogDetail

--- ui/app/app/happyexpat/happyexpat.component.ts
+++ ui/app/app/happyexpat/happyexpat.component.ts
@@ -1,47 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { ExpatriationService } from '../../_services/expatriation.service';
-import { ToastHelper } from '../../_helpers/toast.helper';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'oe-happyexpat',
-  templateUrl: './happyexpat.component.html',
-  styleUrls: ['./happyexpat.component.scss']
+  template: '<router-outlet></router-outlet>'
 })
-export class HappyexpatComponent implements OnInit {
-
-  public expatriations: any = [];
-  public expatSelected: any;
-
-  constructor(private _expatriationService: ExpatriationService) { }
-
-  ngOnInit() {
-      this.expatriations = this._expatriationService.expatriations;
-      this._loadExpatriations();
-  }
-
-  onListChange() {
-      this._expatriationService.update(this.expatSelected)
-          .subscribe(expatriations => {
-              //NO MESSAGE
-          }, (err) => {
-              ToastHelper.displayError(err);
-          });
-  }
-
-  private _loadExpatriations() {
-      this._expatriationService.getAll().subscribe(expatriations => {
-          this.expatriations = expatriations;
-          const filtred = this.expatriations.filter(v => new Date(v.date) > new Date());
-          if (filtred.length > 0) {
-              this.expatSelected = filtred.reduce((prev, current) => {
-                  return (new Date(current.date) < new Date(prev.date)) ? current : prev
-              });
-          } else if (this.expatriations.length > 0) {
-              this.expatSelected = this.expatriations[0]
-          }
-      }, (err) => {
-          ToastHelper.displayError(err);
-      })
-  }
+export class HappyexpatComponent {
 
 }
