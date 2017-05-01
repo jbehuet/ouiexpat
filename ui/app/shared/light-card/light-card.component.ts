269b2a12134122b2bd94bf399a9db92ecf37347d
cDaffe <atakikoo@gmail.com>
2017-05-01 19:04:22 +0200

    clean add structure to lightCard component

--- ui/app/shared/light-card/light-card.component.ts
+++ ui/app/shared/light-card/light-card.component.ts
@@ -1,21 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 
 @Component({
   selector: 'oe-light-card',
   templateUrl: './light-card.component.html',
   styleUrls: ['./light-card.component.scss']
 })
-export class LightCardComponent implements OnInit {
+export class LightCardComponent {
 
-  constructor() { }
+    @Input() entity: any;
+    @Output() addOrRemoveToFavChange: EventEmitter<any> = new EventEmitter();
+    @Output() isFavChange: EventEmitter<any> = new EventEmitter();
+    @Output() isLikedChange: EventEmitter<any> = new EventEmitter();
+    @Output() likeOrDislikeChange: EventEmitter<any> = new EventEmitter();
+    @Output() visitChange: EventEmitter<any> = new EventEmitter();
 
-  ngOnInit() {
-  }
 
-  visit(entity) {
-    if (!(/^https?:\/\//).test(entity.link))
-      entity.link = 'http://' + entity.link
-    window.open(entity.link);
+    addOrRemoveToFav(entity){
+        this.addOrRemoveToFavChange.emit(entity);
+    }
+
+    isFav(entity){
+        this.isFavChange.emit(entity);
+    }
+
+    isLiked(entity) {
+        this.isLikedChange.emit(entity);
+    }
+
+    likeOrDislike(entity) {
+        this.likeOrDislikeChange.emit(entity);
+    }
+
+    visit(entity) {
+        this.visitChange.emit(entity);
   }
 
 }
