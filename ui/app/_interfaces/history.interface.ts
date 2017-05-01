072786ae87c576fa7390fd5a8ae3c1deebc06c58
jbehuet <jbehuet@gmail.com>
2017-05-01 19:48:13 +0200

    Update user.interface

---
+++ ui/app/_interfaces/history.interface.ts
@@ -0,0 +1,6 @@
+
+export interface History {
+  type: String;
+  date: Date;
+  details: String;
+}
--- ui/app/_interfaces/user.interface.ts
+++ ui/app/_interfaces/user.interface.ts
@@ -1,16 +1,18 @@
+import { Favorites } from './favorites.interface';
+import { History } from './history.interface';
 
-import {Favorites} from './favorites.interface';
 export interface User {
-    _id?: String;
-    firstname: String;
-    lastname: String;
-    password?: String;
-    email: String;
-    photo: String;
-    address?: any;
-    administrator?: boolean;
-    favorites: Favorites;
-    phone?: any;
-    birthday?: Date;
-    history?: any;
+  _id?: String;
+  firstname: String;
+  lastname: String;
+  password?: String;
+  email: String;
+  photo: String;
+  situation: string;
+  address?: any;
+  administrator?: boolean;
+  favorites: Favorites;
+  phone?: any;
+  birthday?: Date;
+  history?: History;
 }
