e54dd6cd39da1752fc7d038a5fa6388cfc3accf6
jbehuet <jbehuet@gmail.com>
2017-05-02 14:21:23 +0200

    edit reverse pipe if nut or empty value

--- ui/app/_pipes/reverse.pipe.ts
+++ ui/app/_pipes/reverse.pipe.ts
@@ -3,6 +3,7 @@ import { Pipe } from '@angular/core'
 @Pipe({ name: 'reverse' })
 export class ReversePipe {
   transform(value: any[]) {
+    if (!value) return []
     return value.slice().reverse();
   }
 }
