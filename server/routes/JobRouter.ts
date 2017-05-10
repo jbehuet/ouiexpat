359011648722d9ac4d7e537267418e6c43800d37
jbehuet <jbehuet@gmail.com>
2017-05-10 11:41:06 +0200

    Add add/remove job from favorites

--- server/routes/JobRouter.ts
+++ server/routes/JobRouter.ts
@@ -2,16 +2,22 @@ import * as mongoose from 'mongoose';
 import * as _ from 'lodash';
 import {Router, Response, NextFunction} from 'express';
 import HTTPCode from '../constants/HttpCodeConstant';
+import HistoryType from '../constants/HistoryTypeConstant';
 import IRequest from '../interfaces/IRequest';
 import AbstractRouter from './AbstractRouter';
 import ErrorHelper from '../helpers/ErrorHelper';
 import JobModel from '../models/JobModel';
+import UserModel from '../models/UserModel';
 import JobFormat from '../formats/JobFormat';
+import UserFormat from '../formats/UserFormat';
+import HistoryFormat from '../formats/HistoryFormat';
 
 class JobRouter extends AbstractRouter {
 
     constructor() {
         super(JobModel)
+        this.router.post('/:job_id/favorites', this.addToFavorites.bind(this));
+        this.router.delete('/:job_id/favorites', this.removeFromFavorites.bind(this));
     }
 
     protected create(req: IRequest, res: Response, next: NextFunction) {
@@ -53,6 +59,64 @@ class JobRouter extends AbstractRouter {
                 res.status(HTTPCode.success.OK).json({ status: HTTPCode.success.OK });
         });
 
+    }
+
+    private addToFavorites(req: IRequest, res: Response, next: NextFunction) {
+
+      UserModel.findOneAndUpdate({ _id: req.authenticatedUser._id },
+        { $push: { 'favorites.jobs': req.params.job_id } }, { new: true }, (err: mongoose.Error, user: UserFormat) => {
+          if (err)
+            ErrorHelper.handleMongooseError(err, res, req);
+          else if (!user)
+            res.status(HTTPCode.error.client.NOT_FOUND).json({ status: HTTPCode.error.client.NOT_FOUND });
+          else {
+
+            JobModel.findOne({ _id: req.params.job_id }, (err: mongoose.Error, job: JobFormat) => {
+
+              if (err)
+                ErrorHelper.handleMongooseError(err, res, req);
+              else if (!job)
+                res.status(HTTPCode.error.client.NOT_FOUND).json({ status: HTTPCode.error.client.NOT_FOUND });
+              else {
+                const history = <HistoryFormat>{ type: HistoryType.FAVORITES, details: job.title + " ajouté aux favoris !" };
+
+                UserModel.saveToHistory(req.authenticatedUser._id, history).then((user) => {
+                  res.status(HTTPCode.success.OK).json({ status: HTTPCode.success.OK, data: user });
+                }).catch(err => {
+                  ErrorHelper.handleMongooseError(err, res, req);
+                })
+              }
+            })
+
+          }
+        });
+    }
+
+    private removeFromFavorites(req: IRequest, res: Response, next: NextFunction) {
+
+      UserModel.findOneAndUpdate({ _id: req.authenticatedUser._id },
+        { $pull: { 'favorites.jobs': req.params.job_id } }, { new: true })
+        .populate('favorites.associations')
+        .populate('favorites.blogs')
+        .populate('favorites.jobs')
+        .exec((err: mongoose.Error, user: UserFormat) => {
+          if (err)
+            ErrorHelper.handleMongooseError(err, res, req);
+          else if (!user)
+            res.status(HTTPCode.error.client.NOT_FOUND).json({ status: HTTPCode.error.client.NOT_FOUND });
+          else {
+            JobModel.findOne({ _id: req.params.job_id }, (err: mongoose.Error, job: JobFormat) => {
+
+              if (err)
+                ErrorHelper.handleMongooseError(err, res, req);
+              else if (!job)
+                res.status(HTTPCode.error.client.NOT_FOUND).json({ status: HTTPCode.error.client.NOT_FOUND });
+              else {
+                res.status(HTTPCode.success.OK).json({ status: HTTPCode.success.OK, data: user });
+              }
+            })
+          }
+        });
     }
 
 
