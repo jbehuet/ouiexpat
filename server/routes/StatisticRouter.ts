aa4bb1e4ede1f999ccfe6ade90e153d69f409889
jbehuet <jbehuet@gmail.com>
2017-08-10 10:35:58 +0200

    Admin dashboard and sidenav

--- server/routes/StatisticRouter.ts
+++ server/routes/StatisticRouter.ts
@@ -1,6 +1,6 @@
 import * as mongoose from 'mongoose';
 import * as _ from 'lodash';
-import {Router, Response, NextFunction} from 'express';
+import { Router, Response, NextFunction } from 'express';
 import HTTPCode from '../constants/HttpCodeConstant';
 
 import IRequest from '../interfaces/IRequest';
@@ -12,6 +12,12 @@ import UserModel from '../models/UserModel';
 
 import ExpatriationModel from '../models/ExpatriationModel';
 import ExpatriationFormat from '../formats/ExpatriationFormat';
+
+import BlogModel from '../models/BlogModel';
+import BlogFormat from '../formats/BlogFormat';
+
+import AssociationModel from '../models/AssociationModel';
+import AssociationFormat from '../formats/AssociationFormat';
 
 class StatisticRouter extends AbstractRouter {
 
@@ -37,25 +43,52 @@ class StatisticRouter extends AbstractRouter {
 
           if (err) ErrorHelper.handleMongooseError(err, res, req);
           else {
-            
-            const data = {
-              users: {
-                count: {
-                  total: users.length,
-                  month: users.filter(user => user.createdAt >= firstDay && user.createdAt <= lastDay).length
-                },
-                last: users[0]
-              },
-              expatriations: {
-                count: {
-                  total: expatriations.length,
-                  month: expatriations.filter(expatriation => expatriation.createdAt >= firstDay && expatriation.createdAt <= lastDay).length
-                },
-                last: expatriations[0]
-              }
-            };
 
-            res.status(HTTPCode.success.OK).json({ status: HTTPCode.success.OK, data });
+            BlogModel.find({}).sort({ "likes": -1 }).exec((err: mongoose.Error, blogs: Array<BlogFormat>) => {
+
+              if (err) ErrorHelper.handleMongooseError(err, res, req);
+              else {
+
+                AssociationModel.find({}).sort({ "likes": -1 }).exec((err: mongoose.Error, associations: Array<AssociationFormat>) => {
+
+                  if (err) ErrorHelper.handleMongooseError(err, res, req);
+                  else {
+                    const data = {
+                      users: {
+                        count: {
+                          total: users.length,
+                          month: users.filter(user => user.createdAt >= firstDay && user.createdAt <= lastDay).length
+                        },
+                        last: users[0]
+                      },
+                      expatriations: {
+                        count: {
+                          total: expatriations.length,
+                          month: expatriations.filter(expatriation => expatriation.createdAt >= firstDay && expatriation.createdAt <= lastDay).length
+                        },
+                        last: expatriations[0]
+                      },
+                      blogs: {
+                        count: {
+                          total: blogs.length,
+                          month: blogs.filter(blog => blog.createdAt >= firstDay && blog.createdAt <= lastDay).length
+                        },
+                        last: blogs[0]
+                      },
+                      associations: {
+                        count: {
+                          total: associations.length,
+                          month: associations.filter(association => association.createdAt >= firstDay && association.createdAt <= lastDay).length
+                        },
+                        last: associations[0]
+                      }
+                    };
+
+                    res.status(HTTPCode.success.OK).json({ status: HTTPCode.success.OK, data });
+                  }
+                })
+              }
+            })
           }
 
         })
--- ui/app/admin/admin.component.ts
+++ ui/app/admin/admin.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '../_services/authentication.service';
+import { StatisticService } from '../_services/statistics.service';
 import { User } from '../_interfaces/user.interface';
+
+import { ToastHelper } from '../_helpers/toast.helper';
 
 @Component({
   selector: 'oe-admin',
@@ -10,14 +13,26 @@ import { User } from '../_interfaces/user.interface';
 export class AdminComponent implements OnInit {
 
   public user: User;
+  public statistics: any;
 
-  constructor(private _authenticationService:AuthenticationService) { }
+  constructor(
+    private _authenticationService:AuthenticationService,
+    private _statisticService: StatisticService
+  ) { }
 
   ngOnInit() {
     this.user = this._authenticationService.user;
     this._authenticationService.userChange.subscribe(
         user => this.user = user
     );
+
+this.statistics = this._statisticService.statistics;
+      this._statisticService.getAll().subscribe(statistics => {
+          this.statistics = statistics;
+      }, (err) => {
+          ToastHelper.displayError(err);
+      })
+
   }
 
 }
